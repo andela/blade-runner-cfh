7100e1bc5afd5a782a707aea9f0233adff778517
SEUNAGBEYE <agbeyeseun1@gmail.com>
2018-01-03 23:14:57 +0100

    feature(landing page design): landing page design
    make page more responsive
    
    [Start #153741758]

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,13 @@
 // $(window).on('load', () => {
 // AOS.refresh();
 // });
+
+$(window).scroll(function(){
+    if($(window).scrollTop() > 100){
+        $('.navbar').addClass('scrolled bg-blue');
+    }else{
+        $('.navbar').removeClass('scrolled bg-blue');
+    }
+
+});
+
