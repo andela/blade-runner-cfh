f978710fe4682fef80614432d1463e986e649800
SEUNAGBEYE <agbeyeseun1@gmail.com>
2018-01-05 17:28:29 +0100

    feature(landing page design):landing page animations
    - complete landing page
    animations
    -fix Hound-ci issues
    
    [Finishes #153741758]

--- config/env/all.js
+++ config/env/all.js
@@ -1,3 +1,6 @@
+var config = require('dotenv');
+config.config();
+
 var path = require('path'),
 rootPath = path.normalize(__dirname + '/../..');
 var keys = rootPath + '/keys.txt';
@@ -5,5 +8,5 @@ var keys = rootPath + '/keys.txt';
 module.exports = {
 	root: rootPath,
 	port: process.env.PORT || 3000,
-    db: process.env.MONGOHQ_URL
+    db: process.env.MONGOHQ_URL || 'mongodb://localhost:27017'
 };
