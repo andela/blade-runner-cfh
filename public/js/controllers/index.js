579f73da54134e2a989a39328dafad30c372bed7
Sanni Kehinde <kensanni@gmail.com>
2018-01-08 16:29:30 +0100

    feature(UI): make error message uniform
    [#153741756]
    
    chore(eslint): remove globals configuration from eslint
    
    [#153741756]
    
    chore(cleanup): remove console.log call from functions
    
    [#153741756]
    
    chore(test): modify test to use mock data
    
    [#153741756]
    
    chore(test): add more assertion to test
    
    [#153741756]

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,58 +1,48 @@
 angular.module('mean.system')
-.controller('IndexController', ['$scope', '$window', 'Global', '$http' ,'$location', 'socket', 'game', 'AvatarService',
-  function ($scope, $window, Global, $http, $location, socket, game, AvatarService) {
-    $scope.global = Global;
-    $scope.data = {};
-    $scope.serverErrors = {};
-
-    $scope.playAsGuest = function() {
-      game.joinGame();
-      $location.path('/app');
-    };
-
-    $scope.showError = function() {
-      if ($location.search().error) {
-        return $location.search().error;
-      } else {
-        return false;
-      }
-    };
-
-    const setAuthorizationToken = function (token) {
-      if (token) {
-        $http.defaults.headers.common.Authorization = token;
-      } else {
-        delete $http.defaults.headers.common.Authorization;
-      }
-    }
-
-    $scope.hasError = (field) => {
-      return $scope.serverErrors[field] !== undefined;
-    }
-
-    $scope.signUp = () => {
-      const successCallback = (res) => {
-        const { token } = res.data;
-        if (token) {
-          $window.localStorage.setItem('token', token);
-          setAuthorizationToken(token);
-        }
-        $location.path('/');
-      };
+  .controller('IndexController', ['$scope', '$window', 'Global', '$http', '$location', 'socket', 'game', 'AvatarService',
+    function ($scope, $window, Global, $http, $location, socket, game, AvatarService) {
+      $scope.global = Global;
+      $scope.data = {};
       $scope.serverErrors = {};
-      const errorCallBack = (error) => {
-        const errorsFromServer = error.data.errors;
-        errorsFromServer.forEach(errorObject => {
-          $scope.serverErrors[errorObject.field] = errorObject.message;
-        });
+
+      $scope.playAsGuest = function () {
+        game.joinGame();
+        $location.path('/app');
       };
-      $http.post('/api/users', $scope.data).then(successCallback, errorCallBack);
-    }
 
-    $scope.avatars = [];
-    AvatarService.getAvatars()
-      .then(function(data) {
-        $scope.avatars = data;
-      });
+      $scope.showError = function () {
+        if ($location.search().error) {
+          return $location.search().error;
+        } else {
+          return false;
+        }
+      };
 
-}]);
+      $scope.hasError = (field) => {
+        return $scope.serverErrors[field] !== undefined;
+      };
+
+      $scope.signUp = () => {
+        const successCallback = (res) => {
+          const { token } = res.data;
+          if (token) {
+            $window.localStorage.setItem('token', token);
+          }
+          $location.path('/');
+        };
+        $scope.serverErrors = {};
+        const errorCallBack = (error) => {
+          const errorsFromServer = error.data.errors;
+          errorsFromServer.forEach((errorObject) => {
+            $scope.serverErrors[errorObject.field] = errorObject.message;
+          });
+        };
+        $http.post('/api/users', $scope.data).then(successCallback, errorCallBack);
+      };
+
+      $scope.avatars = [];
+      AvatarService.getAvatars()
+        .then(function (data) {
+          $scope.avatars = data;
+        });
+    }]);
--- test/feature/signUp.js
+++ test/feature/signUp.js
@@ -2,9 +2,24 @@ const chai = require('chai'),
   mongoose = require('mongoose'),
   chaiHttp = require('chai-http'),
   server = require('../../server'),
+  mockData = require('../mockData.json'),
   User = mongoose.model('User');
 
 const { expect } = chai;
+
+const {
+  validUser,
+  missingName,
+  missingEmail,
+  missingPassword,
+  missingAvatar,
+  missingUsername,
+  invalidName,
+  invalidEmail,
+  invalidUsername,
+  invalidPassword,
+  existingUsername,
+} = mockData.Users;
 
 chai.use(chaiHttp);
 
@@ -12,242 +27,169 @@ describe('Testing user endpoint', () => {
   beforeEach((done) => {
     User.remove({}, done);
   });
-  it('It should not sign up a user without a name', (done) => {
+  it('should not sign up a user without a name', (done) => {
     chai.request(server)
       .post('/api/users')
-      .send({
-        name: '',
-        email: 'sannidevs@gmail.com',
-        username: 'developer',
-        password: 'developertest',
-        avatar: 'fdret'
-      })
+      .send(missingName)
       .end((err, res) => {
         const { errors } = res.body;
         if (errors.length >= 1) {
           expect(errors[0].message).equal('name field cannot be empty');
+          expect(errors[0].field).equal('name');
           expect(res).to.have.status(400);
         }
         done();
       });
   });
-  it('It should not sign up a user with a name less than three character', (done) => {
+  it('should not sign up a user with a name less than three character', (done) => {
     chai.request(server)
       .post('/api/users')
-      .send({
-        name: 'le',
-        email: 'sannidevs@gmail.com',
-        username: 'developer',
-        password: 'developertest',
-        avatar: 'fdret'
-      })
+      .send(invalidName)
       .end((err, res) => {
         const { errors } = res.body;
         if (errors.length >= 1) {
           expect(errors[0].message).equal('Name should be atleast three characters');
+          expect(errors[0].field).equal('name');
           expect(res).to.have.status(400);
         }
         done();
       });
   });
-  it('It should not sign up a user without an email', (done) => {
+  it('should not sign up a user without an email', (done) => {
     chai.request(server)
       .post('/api/users')
-      .send({
-        name: 'sannikayz',
-        email: '',
-        username: 'developer',
-        password: 'developertest',
-        avatar: 'fdret'
-      })
+      .send(missingEmail)
       .end((err, res) => {
         const { errors } = res.body;
         if (errors.length >= 1) {
           expect(errors[0].message).equal('Email field cannot be empty');
+          expect(errors[0].field).equal('email');
           expect(res).to.have.status(400);
         }
         done();
       });
   });
-  it('It should not sign up a user with an invalid email', (done) => {
+  it('should not sign up a user with an invalid email', (done) => {
     chai.request(server)
       .post('/api/users')
-      .send({
-        name: 'sannikayz',
-        email: 'ken.com',
-        username: 'developer',
-        password: 'developertest',
-        avatar: 'fdret'
-      })
+      .send(invalidEmail)
       .end((err, res) => {
         const { errors } = res.body;
         if (errors.length >= 1) {
           expect(errors[0].message).equal('Please input a valid Email address');
+          expect(errors[0].field).equal('email');
           expect(res).to.have.status(400);
         }
         done();
       });
   });
-  it('It should not sign up a user without a username', (done) => {
+  it('should not sign up a user without a username', (done) => {
     chai.request(server)
       .post('/api/users')
-      .send({
-        name: 'sannikayz',
-        email: 'sannidevsss@gmail.com',
-        username: '',
-        password: 'developertest',
-        avatar: 'fdret'
-      })
+      .send(missingUsername)
       .end((err, res) => {
         const { errors } = res.body;
         if (errors.length >= 1) {
           expect(errors[0].message).equal('Username field cannot be empty');
+          expect(errors[0].field).equal('username');
           expect(res).to.have.status(400);
         }
         done();
       });
   });
-  it('It should not sign up a user with a username of less than six characters', (done) => {
+  it('should not sign up a user with a username of less than six characters', (done) => {
     chai.request(server)
       .post('/api/users')
-      .send({
-        name: 'sannikayz',
-        email: 'sannidevsss@gmail.com',
-        username: 'ken',
-        password: 'developertest',
-        avatar: 'fdret'
-      })
+      .send(invalidUsername)
       .end((err, res) => {
         const { errors } = res.body;
         if (errors.length >= 1) {
           expect(errors[0].message).equal('Please input a valid user with atleast 6 characters');
+          expect(errors[0].field).equal('username');
           expect(res).to.have.status(400);
         }
         done();
       });
   });
-  it('It should not sign up a user without a password', (done) => {
+  it('should not sign up a user without a password', (done) => {
     chai.request(server)
       .post('/api/users')
-      .send({
-        name: 'sannikayz',
-        email: 'sannidevs@gmail.com',
-        username: 'developer',
-        password: '',
-        avatar: 'fdret'
-      })
+      .send(missingPassword)
       .end((err, res) => {
         const { errors } = res.body;
         if (errors.length >= 1) {
           expect(errors[0].message).equal('Password field cannot be empty');
+          expect(errors[0].field).equal('password');
           expect(res).to.have.status(400);
         }
         done();
       });
   });
-  it('It should not sign up a user without a password less than eight characters', (done) => {
+  it('should not sign up a user with a password less than eight characters', (done) => {
     chai.request(server)
       .post('/api/users')
-      .send({
-        name: 'sannikayz',
-        email: 'sannidevs@gmail.com',
-        username: 'developer',
-        password: 'ken',
-        avatar: 'fdret'
-      })
+      .send(invalidPassword)
       .end((err, res) => {
         const { errors } = res.body;
         if (errors.length >= 1) {
           expect(errors[0].message).equal('Please input a valid password with atleast 8 characters');
+          expect(errors[0].field).equal('password');
           expect(res).to.have.status(400);
         }
         done();
       });
   });
-  it('It should not sign up a user without an avatar', (done) => {
+  it('should not sign up a user without an avatar', (done) => {
     chai.request(server)
       .post('/api/users')
-      .send({
-        name: 'sannikayz',
-        email: 'sannidevs@gmail.com',
-        username: 'developer',
-        password: 'developertest',
-        avatar: ''
-      })
+      .send(missingAvatar)
       .end((err, res) => {
         const { errors } = res.body;
         if (errors.length >= 1) {
           expect(errors[0].message).equal('Avatar must be selected.');
+          expect(errors[0].field).equal('avatar');
           expect(res).to.have.status(400);
         }
         done();
       });
   });
-  it('It should sign up a  new user without giving errors', (done) => {
+  it('should sign up a  new user without giving errors', (done) => {
     chai.request(server)
       .post('/api/users')
-      .send({
-        name: 'sannikayz',
-        email: 'sanjuyuhhbnetguj@gmail.com',
-        username: 'devlohhkjhurp streahk',
-        password: 'developertest',
-        avatar: 'fdret'
-      })
+      .send(validUser)
       .end((err, res) => {
         expect(res.body.message).equal('User created');
         expect(res).to.have.status(201);
         done();
       });
   });
-  it('It should not signup a new user with existing email', (done) => {
-    User.create({
-      name: 'sannikayz',
-      email: 'sannidevs@gmail.com',
-      username: 'developer',
-      password: 'developertest',
-      avatar: 'fdret'
-    }, () => {
+  it('should not signup a new user with existing email', (done) => {
+    User.create(validUser, () => {
       chai.request(server)
         .post('/api/users')
-        .send({
-          name: 'sannikayz',
-          email: 'sannidevs@gmail.com',
-          username: 'developer',
-          password: 'developertest',
-          avatar: 'fdret'
-        })
+        .send(validUser)
         .end((err, res) => {
           const { errors } = res.body;
           if (errors.length >= 1) {
             expect(errors[0].message).equal('Email has already be chosen');
-            expect(res).to.have.status(400);
+            expect(errors[0].field).equal('email');
+            expect(res).to.have.status(409);
           }
           done();
         });
     });
   });
-  it('It should not signup a new user with existing username', (done) => {
-    User.create({
-      name: 'sannikayz',
-      email: 'sannidev@gmail.com',
-      username: 'developer',
-      password: 'developertest',
-      avatar: 'fdret'
-    }, () => {
+  it('should not signup a new user with existing username', (done) => {
+    User.create(validUser, () => {
       chai.request(server)
         .post('/api/users')
-        .send({
-          name: 'sannikayz',
-          email: 'sannidevs@gmail.com',
-          username: 'developer',
-          password: 'developertest',
-          avatar: 'fdret'
-        })
+        .send(existingUsername)
         .end((err, res) => {
           const { errors } = res.body;
           if (errors.length >= 1) {
             expect(errors[0].message).equal('Username has already been chosen');
-            expect(res).to.have.status(400);
+            expect(errors[0].field).equal('username');
+            expect(res).to.have.status(409);
           }
           done();
         });
