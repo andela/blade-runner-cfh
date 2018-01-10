c78cd5dba67222637153acf1e3b97b0f4499f031
Kati Frantz <frantzenterprises777@gmail.com>
2018-01-10 18:11:57 +0100

    bug(integration tests): fix tests for signUp and signIn

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,14 +1,14 @@
 /**
  * Module dependencies.
  */
-const mongoose = require('mongoose');
-const async = require('async');
-const _ = require('underscore');
 
 /**
  * Redirect users to /#!/app (forcing Angular to reload the page)
+ * @param {obj} req express request object
+ * @param {obg} res express response object
+ * @returns {redirect} redirect user
  */
-exports.play = function (req, res) {
+exports.play = (req, res) => {
   if (Object.keys(req.query)[0] === 'custom') {
     res.redirect('/#!/app?custom');
   } else {
@@ -16,7 +16,7 @@ exports.play = function (req, res) {
   }
 };
 
-exports.render = function (req, res) {
+exports.render = (req, res) => {
   res.render('index', {
     user: req.user ? JSON.stringify(req.user) : 'null'
   });
