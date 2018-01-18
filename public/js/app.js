bdb5224020764c087098406c7768adc565546395
Gbenga Oshinaga <andeladeveloper@Andelas-MacBook-Pro.local>
2018-01-18 01:15:54 +0100

    feature(view-game-log): add restriction to dashboard route add restriction to dashboard route so only authenticated users can access it [#153741768]

--- public/js/app.js
+++ public/js/app.js
@@ -1,55 +1,78 @@
+/* eslint func-names: ["error", "never"] */
+
 angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.route', 'mean.system', 'mean.directives'])
   .config(['$routeProvider',
-      function($routeProvider) {
-          $routeProvider.
-          when('/', {
-            templateUrl: 'views/index.html'
-          }).
-          when('/app', {
-            templateUrl: '/views/app.html',
-          }).
-          when('/privacy', {
-            templateUrl: '/views/privacy.html',
-          }).
-          when('/bottom', {
-            templateUrl: '/views/bottom.html'
-          }).
-          when('/signin', {
-            templateUrl: '/views/signin.html'
-          }).
-          when('/signup', {
-            templateUrl: '/views/signup.html'
-          }).
-          when('/dashboard', {
-            templateUrl: '/views/dashboard.html'
-          }).
-          when('/choose-avatar', {
-            templateUrl: '/views/choose-avatar.html'
-          }).
-          otherwise({
-            redirectTo: '/'
-          });
-      }
-  ]).config(['$locationProvider',
-    function($locationProvider) {
-        $locationProvider.hashPrefix("!");
+    function ($routeProvider) {
+      $routeProvider
+        .when('/', {
+          templateUrl: 'views/index.html'
+        })
+        .when('/app', {
+          templateUrl: '/views/app.html',
+        })
+        .when('/privacy', {
+          templateUrl: '/views/privacy.html',
+        })
+        .when('/bottom', {
+          templateUrl: '/views/bottom.html'
+        })
+        .when('/signin', {
+          templateUrl: '/views/signin.html'
+        })
+        .when('/signup', {
+          templateUrl: '/views/signup.html'
+        })
+        .when('/dashboard', {
+          templateUrl: '/views/dashboard.html',
+          resolve: {
+            auth: Authenticate => Authenticate.authenticate()
+          }
+        })
+        .when('/choose-avatar', {
+          templateUrl: '/views/choose-avatar.html'
+        })
+        .otherwise({
+          redirectTo: '/'
+        });
     }
-  ]).run(['$rootScope', function($rootScope) {
-  $rootScope.safeApply = function(fn) {
-    var phase = this.$root.$$phase;
-    if(phase == '$apply' || phase == '$digest') {
-        if(fn && (typeof(fn) === 'function')) {
-            fn();
+  ]).config(['$locationProvider',
+    function ($locationProvider) {
+      $locationProvider.hashPrefix('!');
+    }
+  ]).run(['$rootScope', function ($rootScope) {
+    $rootScope.safeApply = function (fn) {
+      const phase = this.$root.$$phase;
+      if (phase === '$apply' || phase === '$digest') {
+        if (fn && (typeof (fn) === 'function')) {
+          fn();
         }
-    } else {
+      } else {
         this.$apply(fn);
       }
     };
-  }]).run(['DonationService', function (DonationService) {
+  }])
+  .run(['DonationService', function (DonationService) {
     window.userDonationCb = function (donationObject) {
       DonationService.userDonated(donationObject);
     };
-  }]);
+  }])
+  .run(($rootScope, $location) => {
+    // If the route change failed due to authentication error, redirect them out
+    $rootScope.$on('$routeChangeError', (event, current, previous, rejection) => {
+      if (rejection === 'Not Authenticated') {
+        $location.path('/signin');
+      }
+    });
+  })
+  .factory('Authenticate', ($q, $window) => ({
+    authenticate: () => {
+      const token = $window.localStorage.getItem('token');
+      if (token) {
+        return true;
+      }
+      return $q.reject('Not Authenticated');
+    }
+  }));
 
 angular.module('mean.system', []);
 angular.module('mean.directives', []);
--- public/js/controllers/dashboard.js
+++ public/js/controllers/dashboard.js
@@ -1,36 +1,42 @@
 angular.module('mean.system')
-  .controller('DashboardController', ['$scope', '$window', 'routeActions', ($scope, $window, routeActions) => {
-    $scope.routeActions = routeActions;
-    $scope.games = [];
-    $scope.leaderboard = [];
-    $scope.donations = [];
+  .controller('DashboardController', ['$scope', '$window', '$location', 'routeActions',
+    ($scope, $window, $location, routeActions) => {
+      $scope.routeActions = routeActions;
+      $scope.games = [];
+      $scope.leaderboard = [];
+      $scope.donations = [];
 
-    $scope.getGames = () => {
-      const token = $window.localStorage.getItem('token');
-      if (token) {
-        routeActions.getGames('/api/games/history', token)
-          .then((response) => {
-            $scope.games = response.data.result;
-          }, (error) => {
-            $scope.error = error;
-          });
-      }
-    };
+      $scope.getGames = () => {
+        const token = $window.localStorage.getItem('token');
+        if (token) {
+          routeActions.getGames('/api/games/history', token)
+            .then((response) => {
+              $scope.games = response.data.result;
+            }, (error) => {
+              $scope.error = error;
+            });
+        }
+      };
 
-    $scope.getLeaderboard = () => {
-      const token = $window.localStorage.getItem('token');
-      if (token) {
-        routeActions.getLeaderboard('/api/leaderboard', token)
-          .then((response) => {
-            $scope.leaderboard = response.data.result;
-          }, (error) => {
-            $scope.error = error;
-          });
-      }
-    };
+      $scope.getLeaderboard = () => {
+        const token = $window.localStorage.getItem('token');
+        if (token) {
+          routeActions.getLeaderboard('/api/leaderboard', token)
+            .then((response) => {
+              $scope.leaderboard = response.data.result;
+            }, (error) => {
+              $scope.error = error;
+            });
+        }
+      };
 
-    $scope.getData = () => {
-      $scope.getGames();
-      $scope.getLeaderboard();
-    };
-  }]);
+      $scope.getData = () => {
+        $scope.getGames();
+        $scope.getLeaderboard();
+      };
+
+      $scope.signOut = () => {
+        $window.localStorage.removeItem('token');
+        $location.path('/');
+      };
+    }]);
